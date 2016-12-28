37abb4cf748a21e1547742358a77eca1f5929eb9
Hasan Abdel Halim <hasana@gmail.com>
2016-12-28 19:09:05 +0200

    added events to calendar

--- src/main/webapp/app/schedule/schedule.controller.js
+++ src/main/webapp/app/schedule/schedule.controller.js
@@ -1,19 +1,19 @@
-(function() {
+(function () {
     'use strict';
 
     angular
         .module('royalhallsApp')
         .controller('ScheduleController', ScheduleController);
 
-    ScheduleController.$inject = ['$scope', '$state', '$translate', '$rootScope'];
+    ScheduleController.$inject = ['$scope', '$state', '$translate', '$rootScope', 'Event'];
 
-    function ScheduleController ($scope, $state, $translate, $rootScope) {
+    function ScheduleController($scope, $state, $translate, $rootScope, Event) {
         var vm = this;
         vm.eventSources = [];
         vm.uiConfig = {
-            calendar:{
+            calendar: {
                 editable: true,
-                header:{
+                header: {
                     left: 'month agendaWeek agendaDay',
                     center: 'title',
                     right: 'today prev,next'
@@ -25,5 +25,30 @@
         $rootScope.$on('$translateChangeSuccess', function () {
             vm.uiConfig.calendar.lang = $translate.proposedLanguage();
         });
+
+        loadAll();
+
+        function loadAll() {
+            Event.query({}, onSuccess, onError);
+        }
+
+        function onSuccess(data, headers) {
+            vm.queryCount = vm.totalItems;
+            vm.events = data.map(function (event) {
+                var hallName = event.hall ? event.hall.hallName: '';
+                return {
+                    title: event.eventName + '  -  [' + hallName + ']' ,
+                    start: event.eventStartDate,
+                    end: event.eventEndDate
+                }
+            });
+            console.log(vm.events);
+            vm.eventSources.pop();
+            vm.eventSources.push(vm.events);
+        }
+
+        function onError(error) {
+            AlertService.error(error.data.message);
+        }
     }
 })();
