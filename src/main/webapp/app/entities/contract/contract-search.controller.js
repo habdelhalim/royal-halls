03fffcf362acfa16bd2c2f9af1d44bbd2730d388
Hasan Abdel Halim <hasana@gmail.com>
2016-12-31 00:02:02 +0200

    translation

--- src/main/webapp/app/entities/contract/contract-search.controller.js
+++ src/main/webapp/app/entities/contract/contract-search.controller.js
@@ -7,16 +7,17 @@
 
     ContractSearchController.$inject = ['$timeout', '$scope', '$stateParams', 'ParseLinks', '$uibModalInstance', 'entity', 'Contract', 'Customer', 'Payment', 'Event'];
 
-    function ContractSearchController ($timeout, $scope, $stateParams,  ParseLinks, $uibModalInstance, entity, Contract, Customer, Payment, Event) {
+    function ContractSearchController($timeout, $scope, $stateParams, ParseLinks, $uibModalInstance, entity, Contract, Customer, Payment, Event) {
         var vm = this;
 
         loadAll();
 
-        function loadAll () {
+        function loadAll() {
             Contract.query({
                 page: 0,
                 size: 10,
             }, onSuccess, onError);
+
             function sort() {
                 var result = [vm.predicate + ',' + (vm.reverse ? 'asc' : 'desc')];
                 if (vm.predicate !== 'id') {
@@ -24,12 +25,14 @@
                 }
                 return result;
             }
+
             function onSuccess(data, headers) {
                 vm.links = ParseLinks.parse(headers('link'));
                 vm.totalItems = headers('X-Total-Count');
                 vm.queryCount = vm.totalItems;
                 vm.contracts = data;
             }
+
             function onError(error) {
                 AlertService.error(error.data.message);
             }
@@ -41,11 +44,8 @@
         vm.datePickerOpenStatus = {};
         vm.openCalendar = openCalendar;
         vm.save = save;
-        vm.customers = Customer.query();
-        vm.payments = Payment.query();
-        vm.events = Event.query();
 
-        $timeout(function (){
+        $timeout(function() {
             angular.element('.form-group:eq(1)>input').focus();
         });
 
@@ -53,11 +53,11 @@
             $uibModalInstance.close(item);
         }
 
-        function clear () {
+        function clear() {
             $uibModalInstance.dismiss('cancel');
         }
 
-        function save () {
+        function save() {
             vm.isSaving = true;
             if (vm.contract.id !== null) {
                 Contract.update(vm.contract, onSaveSuccess, onSaveError);
@@ -66,20 +66,20 @@
             }
         }
 
-        function onSaveSuccess (result) {
+        function onSaveSuccess(result) {
             $scope.$emit('royalhallsApp:contractUpdate', result);
             $uibModalInstance.close(result);
             vm.isSaving = false;
         }
 
-        function onSaveError () {
+        function onSaveError() {
             vm.isSaving = false;
         }
 
         vm.datePickerOpenStatus.contractDate = false;
         vm.datePickerOpenStatus.creationDate = false;
 
-        function openCalendar (date) {
+        function openCalendar(date) {
             vm.datePickerOpenStatus[date] = true;
         }
     }
