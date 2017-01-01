cc0721dc00e530eb8f5ebbabd2f295a766cc108a
Hasan Abdel Halim <hasana@gmail.com>
2017-01-01 23:30:38 +0200

    Fixed refresh of optins on contract update

--- src/main/webapp/app/components/contract-options/contract-options.directive.js
+++ src/main/webapp/app/components/contract-options/contract-options.directive.js
@@ -15,33 +15,49 @@
         .module('royalhallsApp')
         .component('contractOptions', contractOptions);
 
-    ContractOptionsController.$inject = ['ContractOption'];
+    ContractOptionsController.$inject = ['$rootScope', 'ContractOption'];
 
-    function ContractOptionsController(ContractOption) {
+    function ContractOptionsController($rootScope, ContractOption) {
         var vm = this;
         vm.options = [];
 
-        if (vm.contract) {
-            ContractOption.query({
-                id: vm.contract.id
-            }, function (entity) {
-                vm.events = entity;
+        var unsubscribe = $rootScope.$on('royalhallsApp:contractUpdate', function () {
+            update();
+        });
 
-                if (vm.events) {
-                    vm.options =
-                        vm.events.map(function (ev) {
-                            var options =
-                                ev.options.map(function (op) {
-                                    op.event = {id: ev.id, eventName: ev.eventName};
-                                    return op;
-                                });
+        this.$onDestroy = function () {
+            if (unsubscribe) {
+                unsubscribe();
+            }
+        };
 
-                            return options;
-                        });
+        update();
 
-                    vm.options = [].concat.apply([], vm.options);
-                }
-            });
+        function update() {
+            if (vm.contract) {
+                ContractOption.query({
+                    id: vm.contract.id
+                }, function (entity) {
+                    vm.events = entity;
+
+                    if (vm.events) {
+                        vm.options =
+                            vm.events.map(function (ev) {
+                                var options =
+                                    ev.options.map(function (op) {
+                                        op.event = {id: ev.id, eventName: ev.eventName};
+                                        return op;
+                                    });
+
+                                return options;
+                            });
+
+                        //flatten array
+                        vm.options = [].concat.apply([], vm.options);
+                    }
+                });
+            }
         }
+
     }
 })();
