3112471b85ce43ad03f1940de8628dd7a798cd0c
Hasan Abdel Halim <hasana@gmail.com>
2017-03-18 00:50:27 +0200

    event prices are now recorded on the option level

--- src/main/webapp/app/components/contract-options/contract-options.directive.js
+++ src/main/webapp/app/components/contract-options/contract-options.directive.js
@@ -49,7 +49,7 @@
                                 var options =
                                     ev.options.map(function (op) {
                                         op.event = {id: ev.id, eventName: ev.eventName};
-                                        op.price = (op.variant === null) ? op.option.price : op.variant.price;
+                                        // op.price = (op.variant === null) ? op.option.price : op.variant.price;
                                         op.total = (op.optionQty === null) ? op.price : op.optionQty * op.price;
 
                                         vm.totalAmount = vm.totalAmount + op.total;
