6e1efe0c87b5d59bb23ba378263b980ef8d8208d
Hasan Abdel Halim <hasana@gmail.com>
2017-07-09 23:10:18 +0300

    ability to set user's password

--- src/main/java/com/royal/app/service/UserService.java
+++ src/main/java/com/royal/app/service/UserService.java
@@ -123,7 +123,7 @@ public class UserService {
             );
             user.setAuthorities(authorities);
         }
-        String encryptedPassword = passwordEncoder.encode(RandomUtil.generatePassword());
+        String encryptedPassword = passwordEncoder.encode(managedUserVM.getPassword());
         user.setPassword(encryptedPassword);
         user.setResetKey(RandomUtil.generateResetKey());
         user.setResetDate(ZonedDateTime.now());
